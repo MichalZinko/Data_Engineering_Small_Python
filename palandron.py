5f562718a4c41ad1c0b316a62b017c04b3a962b3
MZinko <michalzinko@gmail.com>
2024-06-20 18:31:47 +0200

    fix:Capital_letters_errors_and_adding_new_line_for_printed_text

--- palandron.py
+++ palandron.py
@@ -1,11 +1,8 @@
 
-print('Palandron Tester')
-order()
-
 def main_menu():
-    print('Input the word that you want to check')
-    test = input()
-    return test
+    print('\nInput the word that you want to check')
+    user_input = input()
+    return user_input
     
 def palindrom(word):
     starting_letter_index = 0
@@ -13,27 +10,29 @@ def palindrom(word):
     half_of_lenght_of_word = int(len(word) / 2 + len(word) % 2)
     while starting_letter_index <= half_of_lenght_of_word:
         if starting_letter_index == half_of_lenght_of_word:
-            return print("This is a Palindrom")
+            return print("\nThis is a Palindrom")
         else:
             if word[starting_letter_index] != word[ending_letter_index]:
-                return print("This is not a Palindrom")
+                return print("\nThis is not a Palindrom")
         starting_letter_index += 1
         ending_letter_index -= 1
 
 def ending():
-    print('Do you want to continue')
-    print('If yes press Y if No press N')
+    print('\nDo you want check another word')
+    print('If Yes press y if No press n')
     anwser = input()
-    if anwser == 'Y':
+    if anwser.lower() == 'y':
         order()
-    elif anwser == 'N':
+    elif anwser.lower() == 'n':
         exit 
     else:
         print('That is not a valid option')
         ending() 
 
 def order():
-    word = main_menu()
-    palindrom(word)
+    user_input_word = main_menu()
+    palindrom(user_input_word.lower())
     ending()
 
+print('\n~~Palandron Tester~~')
+order()
